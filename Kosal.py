21beb89097576fe98d4e7030de7e58a1ab4a5bf6
unknown <Kosal.Pouy@PNC.LAN>
2024-07-25 09:46:32 +0700

    lesson-1

---
+++ Kosal.py
@@ -0,0 +1,66 @@
+# Challeng 1
+# print("PNC")
+
+# Challeng 2
+# for i in range(5):
+#     print("Hello")
+
+# Challeng3 
+# Different bettween:
+# print(age) result = number
+# print("resut") result = letter
+
+# Challeng4
+# value = 0 
+# while value < 2:
+#     value += 1
+# print(value)
+
+# Challeng 5 
+# age = 19
+# for i in range(age, 30,1):
+#     print(19)
+
+# Challeng6
+# for i in range (10):
+#     print(i+1)
+
+# Challeng7
+# age = 14
+# size = 21
+# print("My age is",age , "and" , "My size is",size)
+
+# Challeng8
+# myNumber = int(input("Enter number:"))
+# text="Hello"
+# for i in range(myNumber):
+#     print(text)
+
+# myNumber = input()
+# for i in range(int(myNumber))
+#     print("Hello")
+
+# is_not_zero = True
+# number = int(input("enter your text:"))
+# while is_not_zero:
+#     if number !="0":
+#         print("Bad")
+#         number = int(input("enter your text again:"))
+#     else:
+#         print("Good")
+#         is_not_zero =False
+
+
+# n = 5
+# text = ""
+# for i in range (n):
+#     text = text + "x"
+#     print(text)
+    
+n = 5
+for i in range(n):
+    space = " " * (4 - u)
+    text = "x" * (u + 1)
+    print(test + text)
+
+
